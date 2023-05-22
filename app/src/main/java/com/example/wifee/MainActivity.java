79a9cec58b3fc5045d11427f83705e748e0cbecb
minseok cho <whalstjr199927@gmail.com>
2023-05-22 22:14:57 +0900

    ar실행 화면 버튼

--- app/src/main/java/com/example/wifee/MainActivity.java
+++ app/src/main/java/com/example/wifee/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.wifee;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton arBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_backup);
+        setContentView(R.layout.activity_main);
+
+        arBtn = (ImageButton) findViewById(R.id.arBtn);
+        arBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openARMainActivity();
+            }
+        });
+    }
+
+    public void openARMainActivity() {
+        Intent intent = new Intent(this, ArMain.class);
+        startActivity(intent);
     }
 }
