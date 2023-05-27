3a5a04fb576278390870e56654bede598a4ed9d4
minseok cho <whalstjr199927@gmail.com>
2023-05-27 16:17:38 +0900

    맵 추가

--- app/src/main/java/com/example/wifee/MainActivity.java
+++ app/src/main/java/com/example/wifee/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.wifee;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageButton arBtn;
+    private ImageButton mapBtn;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +34,27 @@ public class MainActivity extends AppCompatActivity {
                 openARMainActivity();
             }
         });
+
+        mapBtn = (ImageButton) findViewById(R.id.mapBtn);
+        mapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMAPMainActivity();
+            }
+        });
+
+
     }
 
     public void openARMainActivity() {
         Intent intent = new Intent(this, ArMain.class);
         startActivity(intent);
     }
+
+    public void openMAPMainActivity() {
+        Intent intent = new Intent(this, MapMain.class);
+        startActivity(intent);
+    }
+
+
 }
