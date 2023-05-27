d2ef6f8156e9a00146a30c153e82ccaba4545d28
minseok cho <whalstjr199927@gmail.com>
2023-05-27 20:42:34 +0900

    wifi recommend ui

--- app/src/main/java/com/example/wifee/ArMain.java
+++ app/src/main/java/com/example/wifee/ArMain.java
@@ -2,13 +2,32 @@ package com.example.wifee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ArMain extends AppCompatActivity {
+
+    private Button recommendBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ar_main);
+
+        recommendBtn = (Button) findViewById(R.id.arWifiRecommendBtn);
+
+        recommendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openARRecommendActivity();
+            }
+        });
+    }
+
+    public void openARRecommendActivity() {
+        Intent intent = new Intent(this, ArRecommend.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/wifee/ArRecommend.java
@@ -0,0 +1,17 @@
+package com.example.wifee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class ArRecommend extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ar_wifi_recommend);
+
+
+    }
+}
