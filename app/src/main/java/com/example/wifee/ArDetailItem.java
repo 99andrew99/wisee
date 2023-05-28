1c5efdc5e541de5cd1b67a47b488359fa3bc54f8
minseok cho <whalstjr199927@gmail.com>
2023-05-28 11:42:18 +0900

    ui 최종

---
+++ app/src/main/java/com/example/wifee/ArDetailItem.java
@@ -0,0 +1,26 @@
+package com.example.wifee;
+
+public class ArDetailItem {
+    private String ssid;
+    private String signal;
+    private String frequency;
+    private String mac;
+    private String time;
+
+    //생성자
+    public ArDetailItem(String ssid, String signal, String frequency, String mac, String time) {
+        this.ssid = ssid;
+        this.signal = signal;
+        this.frequency = frequency;
+        this.mac = mac;
+        this.time = time;
+    }
+
+    // Getter 메소드
+    public String getSsid() { return ssid; }
+    public String getSignal() { return signal; }
+    public String getFrequency() { return frequency; }
+    public String getMac() { return mac; }
+    public String getTime() { return time; }
+}
+
