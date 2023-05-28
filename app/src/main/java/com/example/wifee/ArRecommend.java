225bcaa2a43d1360970fbe7a4d525389e3b1a515
Dragonfri <rladudrmsghd@gmail.com>
2023-05-28 12:54:15 +0900

    버튼에 따라 정렬기준이 바뀌는 기능 추가

--- app/src/main/java/com/example/wifee/ArRecommend.java
+++ app/src/main/java/com/example/wifee/ArRecommend.java
@@ -8,28 +8,67 @@ import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import kotlin.collections.ArrayDeque;
+
 
 public class ArRecommend extends AppCompatActivity {
 
-
+    private Button recByRSSIBtn;
+    private Button recByFreqBtn;
+    private TextView recNameTxt;
+    private TextView recRSSITxt;
+    private TextView recFreqTxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ar_wifi_recommend);
 
+        recByRSSIBtn = (Button) findViewById(R.id.arRecommendSignalBtn);
+        recByFreqBtn = (Button) findViewById(R.id.arRecommendFrequencyBtn);
+        recNameTxt = (TextView)  findViewById(R.id.arRecommendNameText);
+        recRSSITxt = (TextView) findViewById(R.id.arRecommendSignalText);
+        recFreqTxt = (TextView) findViewById(R.id.arRecommendFrequencyText);
+        recByRSSIBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyWifiInfo bestWifi = getBestWifi(0);
+                recNameTxt.setText(bestWifi.getSSID());
+                recRSSITxt.setText(Integer.toString(bestWifi.getRSSI()));
+                recFreqTxt.setText(Double.toString(bestWifi.getFrequency()));
+            }
+        });
+
+        recByFreqBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyWifiInfo bestWifi = getBestWifi(1);
+                recNameTxt.setText(bestWifi.getSSID());
+                recRSSITxt.setText(Integer.toString(bestWifi.getRSSI()));
+                recFreqTxt.setText(Double.toString(bestWifi.getFrequency()));
+            }
+        });
     }
+
+
 
     private WifiManager getWifiManager() {
         return (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
     }
 
     // get existing wifi list
-    public String[] getWifiList() {
+    public MyWifiInfo getBestWifi(int option) {
         String[] result = new String[0];
 
         WifiManager wifiManager = getWifiManager();
@@ -50,13 +89,36 @@ public class ArRecommend extends AppCompatActivity {
         }
 
         List<ScanResult> scanResults = wifiManager.getScanResults();
-        List<MyWifiInfo> myWifiInfoList;
+        List<MyWifiInfo> myWifiInfoList = new ArrayList<>();
         for (int i = 0; i < scanResults.size(); i++) {
-            myWifiInfoList.add(new MyWifiInfo(scanResults.get(i).SSID,scanResults.get(i).level,scanResults.get(i).))
-            result[i] = scanResults.get(i).;
+            myWifiInfoList.add(new MyWifiInfo(scanResults.get(i).SSID,scanResults.get(i).level,scanResults.get(i).frequency));
         }
 
-        return result;
+        switch (option){
+            case 0: sortWiFiInfoByRSSI(myWifiInfoList);
+            case 1: sortWiFiInfoByFrequency(myWifiInfoList);
+            default: sortWiFiInfoByRSSI(myWifiInfoList);
+        }
+
+
+
+        Log.d("TEST", myWifiInfoList.get(0).getSSID());
+        Log.d("TEST", Integer.toString(myWifiInfoList.get(0).getRSSI()));
+        Log.d("TEST", Double.toString(myWifiInfoList.get(0).getFrequency()));
+
+
+        return myWifiInfoList.get(0);
     }
+
+    private static List<MyWifiInfo> sortWiFiInfoByRSSI(List<MyWifiInfo> wifiInfoList) {
+        Collections.sort(wifiInfoList, Comparator.comparingInt(MyWifiInfo::getRSSI).reversed());
+        return wifiInfoList;
+    }
+
+    private static List<MyWifiInfo> sortWiFiInfoByFrequency(List<MyWifiInfo> wifiInfoList) {
+        Collections.sort(wifiInfoList, Comparator.comparingDouble(MyWifiInfo::getFrequency));
+        return wifiInfoList;
+    }
+
 
 }
--- app/src/main/java/com/example/wifee/MyWifiInfo.java
+++ app/src/main/java/com/example/wifee/MyWifiInfo.java
@@ -4,9 +4,8 @@ public class MyWifiInfo {
     private String ssid;
     private int rssi;
     private double frequency;
-    private String ipAddress;
 
-    public MyWifiInfo(String ssid, int rssi, int speed, double frequency) {
+    public MyWifiInfo(String ssid, int rssi, double frequency) {
         this.ssid = ssid;
         this.rssi = rssi;
         this.frequency = frequency;
